37c054325d34aa494eda9bc84465ca194461d95e
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-13 16:58:10 +0900

    design: Color 정의

--- presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.paykidscompose.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Transparent = Color(0x00000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue1 = Color(0xFF237FFF)
+val Blue2 = Color(0xFF3AA6FF)
+val Blue3 = Color(0xFF52CEFF)
+
+val Red = Color(0xFFFF4655)
+
+val Gray1 = Color(0xFF696B6E)
+val Gray2 = Color(0xFFB2B4B6)
+val Gray3 = Color(0xFFD6DADB)
+val Gray4 = Color(0xFFEFF1F2)
+val Gray5 = Color(0xFFF1F8FF)
+val Gray6 = Color(0xFFC1DEFB)
+val Gray7 = Color(0xFF9ABFF4)
+val Gray8 = Color(0xFF7796BA)
+
+val Black = Color(0xFF191919)
+val White = Color(0xFFFFFFFF)
+
+// 네비게이션 전용 색상
+val BottomNavSelected = Blue1
+val BottomNavUnselected = Gray6
