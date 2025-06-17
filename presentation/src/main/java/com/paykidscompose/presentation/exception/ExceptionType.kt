c6f5c55ec29c0ef3ad16709a3277fc5bb6309bba
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-06-17 22:28:40 +0900

    feat: 스플래시 이후에 나오는 닉네임 입력 화면 추가.

---
+++ presentation/src/main/java/com/paykidscompose/presentation/exception/ExceptionType.kt
@@ -0,0 +1,7 @@
+package com.paykidscompose.presentation.exception
+
+enum class ExceptionType {
+    Snack,
+    Toast,
+    Dialog
+}
---
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/components/InputFieldStyle.kt
@@ -0,0 +1,6 @@
+package com.paykidscompose.presentation.ui.components
+
+enum class InputFieldStyle {
+    Underline,
+    Outline
+}
