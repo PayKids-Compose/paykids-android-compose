6df4179d17090d17db501b20ad91952ae6456beb
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-21 20:52:18 +0900

    feat: 툴팁 구현 및 상단 카드 스테이지 정보 적용

--- presentation/src/main/java/com/paykidscompose/presentation/screens/home/HomeScreen.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/screens/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,21 +26,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 import com.paykidscompose.presentation.R
+import com.paykidscompose.presentation.dummy.getStageTitle
+import com.paykidscompose.presentation.ui.components.ImageTooltip
 import com.paykidscompose.presentation.ui.theme.Blue1
 import com.paykidscompose.presentation.ui.theme.StageCardNumberTextStyle
 import com.paykidscompose.presentation.ui.theme.StageCardTitleTextStyle
@@ -55,6 +64,8 @@ import com.paykidscompose.presentation.ui.theme.StageDescriptionCardTextVertical
 import com.paykidscompose.presentation.ui.theme.StageDescriptionCardVerticalPadding
 import com.paykidscompose.presentation.ui.theme.StageHorizontalPadding
 import com.paykidscompose.presentation.ui.theme.StageIconSize
+import com.paykidscompose.presentation.ui.theme.StageTooltipOffsetX
+import com.paykidscompose.presentation.ui.theme.StageTooltipOffsetY
 import com.paykidscompose.presentation.ui.theme.StageTopPadding
 import com.paykidscompose.presentation.ui.theme.StageVerticalSpace
 import com.paykidscompose.presentation.ui.theme.White
@@ -88,6 +99,11 @@ fun HomeScreen() {
     }
     val context = LocalContext.current
 
+    val tooltipOffset = remember { mutableStateOf<Offset?>(null) }
+
+    val lastClearedStageIndex = 6 // 클리어 스테이지 인덱스
+    val selectedStageIndex = remember { mutableIntStateOf(lastClearedStageIndex) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -101,7 +117,7 @@ fun HomeScreen() {
             filterQuality = FilterQuality.High,
             contentScale = ContentScale.FillWidth,
             clipToBounds = false,
-            alignment = Alignment.TopCenter,
+            alignment = Alignment.TopStart,
             modifier = Modifier
                 .fillMaxWidth()
                 .offset(y = backgroundOffset)
@@ -124,6 +140,7 @@ fun HomeScreen() {
                     unlockedStageCount,
                     stageImageSet
                 )
+                val itemOffset = remember { mutableStateOf(Offset(0f, 0f)) }
 
                 Box(
                     modifier = Modifier
@@ -134,7 +151,16 @@ fun HomeScreen() {
                             color = borderColor,
                             shape = CircleShape
                         )
-                        .clickable {
+                        .onGloballyPositioned { coordinates ->
+                            itemOffset.value = coordinates.positionInWindow()
+                        }
+                        .clickable(
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ) {
+                            selectedStageIndex.intValue = index
+                            tooltipOffset.value = Offset(itemOffset.value.x, itemOffset.value.y)
+
                             if (index < unlockedStageCount) {
 
                             } else {
@@ -145,7 +171,7 @@ fun HomeScreen() {
                 ) {
                     Image(
                         painter = painterResource(id = imageRes),
-                        contentDescription = null,
+                        contentDescription = null, // ripple 제거
                         modifier = Modifier.size(StageIconSize)
                     )
                 }
@@ -180,18 +206,51 @@ fun HomeScreen() {
                     )
             ) {
                 Text(
-                    text = stringResource(R.string.text_stage_number),
+                    text = selectedStageIndex.intValue.let {
+                        stringResource(R.string.text_stage_number, it + 1)
+                    },
                     modifier = Modifier.fillMaxWidth(),
                     style = StageCardNumberTextStyle
                 )
                 Spacer(modifier = Modifier.height(StageDescriptionCardTextSpacer))
                 Text(
-                    text = stringResource(R.string.text_stage_title),
+                    text = selectedStageIndex.intValue?.let { getStageTitle(it) } ?: stringResource(R.string.text_stage_title),
                     modifier = Modifier.fillMaxWidth(),
                     style = StageCardTitleTextStyle
                 )
             }
         }
 
+        // 화면을 클릭하면 툴팁 닫힘
+        if (tooltipOffset.value != null) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable(
+                        onClick = {
+                            tooltipOffset.value = null
+                        },
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    )
+            )
+        }
+
+        // 툴팁 표시
+        tooltipOffset.value?.let { offset ->
+            ImageTooltip(
+                tooltipText = stringResource(R.string.text_tooltip_start),
+                modifier = Modifier
+                    .clickable {
+                        tooltipOffset.value = null
+                    }
+                    .offset {
+                        IntOffset(
+                            offset.x.toInt() - StageTooltipOffsetX,
+                            offset.y.toInt() - StageTooltipOffsetY
+                        )
+                    }
+            )
+        }
     }
 }
