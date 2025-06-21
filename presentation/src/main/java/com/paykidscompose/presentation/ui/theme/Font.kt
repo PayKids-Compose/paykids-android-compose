0ddba91b3a4c59a79dd412ae7fb2da443c4b2905
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-21 20:43:12 +0900

    theme: 툴팁 텍스트 스타일 작성

--- presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Font.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Font.kt
@@ -56,3 +56,10 @@ val StageCardTitleTextStyle = TextStyle(
     fontSize = 18.sp,
     lineHeight = 20.sp
 )
+
+val StageTooltipTextStyle = TextStyle(
+    fontFamily = NanumSquare,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 22.sp,
+    lineHeight = 25.sp
+)
