656b2797c8928ba5810d53d0f8c30032bffaba18
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-21 20:43:51 +0900

    feat: 이미지 툴팁 컴포넌트 작성

---
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/components/TooltipComponents.kt
@@ -0,0 +1,55 @@
+package com.paykidscompose.presentation.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.paykidscompose.presentation.R
+import com.paykidscompose.presentation.ui.theme.Blue1
+import com.paykidscompose.presentation.ui.theme.StageTooltipTextStyle
+
+@Preview
+@Composable
+fun ImageTooltip(
+    modifier: Modifier = Modifier,
+    imageRes: Painter = painterResource(R.drawable.img_tooltip_blank),
+    tooltipText: String = stringResource(R.string.text_tooltip_start),
+    onClickDismiss: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .wrapContentSize()
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { onClickDismiss() },
+    ) {
+        Image(
+            modifier = Modifier.size(156.dp, 80.dp),
+            painter = imageRes,
+            contentDescription = null,
+        )
+
+        Text(
+            text = tooltipText,
+            color = Blue1,
+            style = StageTooltipTextStyle,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = 8.dp)
+        )
+    }
+}
