eb157b03783d2e6170601eb1359f2433a688fb8b
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-22 02:51:38 +0900

    feat: QuizEntryScreen UI 구현

---
+++ presentation/src/main/java/com/paykidscompose/presentation/screens/quiz/QuizEntryScreen.kt
@@ -0,0 +1,130 @@
+package com.paykidscompose.presentation.screens.quiz
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.paykidscompose.presentation.R
+import com.paykidscompose.presentation.ui.components.DecisionButton
+import com.paykidscompose.presentation.ui.theme.Blue2
+import com.paykidscompose.presentation.ui.theme.DeterminationTextStyle2
+import com.paykidscompose.presentation.ui.theme.Gray5
+import com.paykidscompose.presentation.ui.theme.QuizEntryButtonSpacer
+import com.paykidscompose.presentation.ui.theme.QuizEntryScreenBottomPadding
+import com.paykidscompose.presentation.ui.theme.QuizEntryScreenSpacer1
+import com.paykidscompose.presentation.ui.theme.QuizEntryScreenSpacer2
+import com.paykidscompose.presentation.ui.theme.QuizEntryScreenTopPadding
+import com.paykidscompose.presentation.ui.theme.StageNumberCardHorizontalPadding
+import com.paykidscompose.presentation.ui.theme.StageNumberCardRound
+import com.paykidscompose.presentation.ui.theme.StageNumberCardTextStyle
+import com.paykidscompose.presentation.ui.theme.StageNumberCardVerticalPadding
+import com.paykidscompose.presentation.ui.theme.StageTooltipTextStyle
+import com.paykidscompose.presentation.ui.theme.StartAndEndPadding
+import com.paykidscompose.presentation.ui.theme.White
+
+@Preview(showBackground = true)
+@Composable
+fun QuizEntryScreen() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    BackHandler { showDialog = true }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        if (showDialog) {
+
+        }
+
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.bg_quiz_enter),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = StartAndEndPadding)
+                .padding(top = QuizEntryScreenTopPadding, bottom = QuizEntryScreenBottomPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier,
+                shape = RoundedCornerShape(StageNumberCardRound),
+                colors = CardDefaults.cardColors(containerColor = Gray5)
+            ) {
+                Column(
+                    modifier = Modifier.padding(horizontal = StageNumberCardHorizontalPadding, vertical = StageNumberCardVerticalPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "스테이지 3", style = StageNumberCardTextStyle)
+                }
+            }
+            Spacer(modifier = Modifier.height(QuizEntryScreenSpacer1))
+
+            Text(text = "화폐의 단위에 대해 알아보아요", style = StageTooltipTextStyle, color = White)
+            Spacer(modifier = Modifier.height(QuizEntryScreenSpacer2))
+
+            DecisionButton(
+                text = stringResource(R.string.text_btn_study),
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                backgroundColor = White,
+                contentColor = Blue2,
+                textStyle = DeterminationTextStyle2
+            )
+            Spacer(modifier = Modifier.height(QuizEntryButtonSpacer))
+            DecisionButton(
+                text = stringResource(R.string.text_btn_quiz),
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                backgroundColor = Blue2,
+                contentColor = White,
+                textStyle = DeterminationTextStyle2
+            )
+            Spacer(modifier = Modifier.height(QuizEntryButtonSpacer))
+            DecisionButton(
+                text = stringResource(R.string.text_btn_review),
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                backgroundColor = White,
+                contentColor = Blue2,
+                textStyle = DeterminationTextStyle2
+            )
+        }
+
+    }
+}
