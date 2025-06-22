f9435474364aa140b9ea96337a67d139a4e375c2
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-06-23 00:23:23 +0900

    theme: QuizScreen Dimens 작성

--- presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Dimens.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/theme/Dimens.kt
@@ -1,5 +1,6 @@
 package com.paykidscompose.presentation.ui.theme
 
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 // 공통
@@ -12,6 +13,7 @@ val DeterminationButtonTextTopAndBottom = 14.dp
 val DeterminationButtonTextTopAndBottom2 = 15.dp
 val DeterminationButtonWidth = 320.dp
 val DeterminationButtonHeight = 50.dp
+val CardShadowElevation = 8.dp
 
 // 스플래시 화면
 val SplashLogo = 174.dp
@@ -40,7 +42,6 @@ val StageDescriptionCardHeight = 80.dp
 val StageDescriptionCardTextHorizontalPadding = 16.dp
 val StageDescriptionCardTextVerticalPadding = 20.dp
 val StageDescriptionCardTextSpacer = 7.dp
-val StageDescriptionCardShadowElevation = 8.dp
 
 val StageTooltipImageWidth = 156.dp
 val StageTooltipImageHeight = 80.dp
@@ -57,6 +58,16 @@ val StageNumberCardVerticalPadding = 6.dp
 val QuizEntryScreenSpacer1 = 18.dp
 val QuizEntryScreenSpacer2 = 50.dp
 val QuizEntryButtonSpacer = 10.dp
+
+// 퀴즈 화면
+val QuizQuestionTextSpacer = 80.dp
+val ImageQuizCardSpaceBetween = 20.dp
+val ImageQuizCardSize = Pair<Dp, Dp>(150.dp, 180.dp)
+val ImageQuizCardRound = 20.dp
+val ImageQuizCardImageSize = 120.dp
+val ImageQuizCardImageRound = 10.dp
+val ImageQuizCardTextSpacer = 12.dp
+val ImageQuizCardRowSpacer = 14.dp
 
 // 마이페이지 기본 화면
 val MyPageDefaultScreenStartEndPadding = 20.dp
