bcfe4cbd0f6b986d7b5d10ea6c7d55c94f85d1f9
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-07-08 20:49:55 +0900

    refactor: ImageTooltip 컴포넌트 offset 파라미터 적용 부분 제거(외부에서 적용)

--- presentation/src/main/java/com/paykidscompose/presentation/ui/components/TooltipComponents.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/ui/components/TooltipComponents.kt
@@ -1,21 +1,17 @@
 package com.paykidscompose.presentation.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import com.paykidscompose.presentation.R
 import com.paykidscompose.presentation.ui.theme.Blue1
 import com.paykidscompose.presentation.ui.theme.StageTooltipImageHeight
@@ -27,24 +23,12 @@ import com.paykidscompose.presentation.ui.theme.StageTooltipTextStyle
 @Composable
 fun ImageTooltip(
     modifier: Modifier = Modifier,
-    offset: DpOffset = DpOffset.Zero,
     imageRes: Painter = painterResource(R.drawable.img_tooltip_blank),
-    tooltipText: String = stringResource(R.string.text_tooltip_start),
-    onClickDismiss: () -> Unit = {}
+    tooltipText: String = stringResource(R.string.text_tooltip_start)
 ) {
-    val adjustedOffset = DpOffset(
-        x = offset.x - StageTooltipImageWidth / 2,
-        y = offset.y
-    )
-
     Box(
         modifier = modifier
-            .size(StageTooltipImageWidth, StageTooltipImageHeight)
-            .offset(adjustedOffset.x, adjustedOffset.y)
-            .clickable(
-                indication = null,
-                interactionSource = remember { MutableInteractionSource() }
-            ) { onClickDismiss() },
+            .size(StageTooltipImageWidth, StageTooltipImageHeight),
         contentAlignment = Alignment.Center
     ) {
         Image(
