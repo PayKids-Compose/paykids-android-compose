65c777d691192fdb1eabbd4ba9b2375842999c59
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-07-14 04:03:42 +0900

    feat: ChatResponseDTO 작성

---
+++ data/src/main/java/com/paykidscompose/data/model/study/ChatResponseDTO.kt
@@ -0,0 +1,8 @@
+package com.paykidscompose.data.model.study
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ChatResponseDTO(
+    val response: Map<String, String>
+)
