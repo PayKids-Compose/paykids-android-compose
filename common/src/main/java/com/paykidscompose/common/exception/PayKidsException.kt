48a4108a072651e1da71174fa95131d487d22bfc
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-07-17 02:58:04 +0900

    feat: common에 예외 처리 넣음. 데이터랑 프레젠테이션 레이어가 공통으로 사용 할 수 있는 예외.
    데이터가 안드로이드 라이브러리를 사용하고 있기 때문에 뷰모델에서 예외 처리하지 않고 데이터레이어에서 스낵바와 토스트를 띄우는 것도 가능하기 때문에 common에 넣었습니다.

---
+++ common/src/main/java/com/paykidscompose/common/exception/PayKidsException.kt
@@ -0,0 +1,21 @@
+package com.paykidscompose.common.exception
+
+import com.paykidscompose.common.enums.ExceptionType
+
+sealed class PayKidsException(
+    open val code: Int,
+    val type: ExceptionType,
+    override val message: String?
+) : Throwable(message) {
+
+    data class SnackBarException(
+        override val code: Int = -1,
+        override val message: String
+    ) : PayKidsException(code, ExceptionType.Snack, message)
+
+    data class ToastException(
+        override val code: Int,
+        override val message: String
+    ) : PayKidsException(code, ExceptionType.Toast, message)
+
+}
