423d05525df981826b96e151a257d9dd89d48d04
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-07-17 02:09:19 +0900

    refactor: 소비 카테고리 리포지토리 수정
    인터페이스를 구현하도록 수정함.
    
    get에서 DTO 반환을 common 모델 반환으로 변경

--- data/src/main/java/com/paykidscompose/data/repositories/ExpenseCategoryRepositoryImpl.kt
+++ data/src/main/java/com/paykidscompose/data/repositories/ExpenseCategoryRepositoryImpl.kt
@@ -1,15 +1,20 @@
 package com.paykidscompose.data.repositories
 
+import com.paykidscompose.common.model.allowance.CategoryModel
+import com.paykidscompose.common.repositories.ExpenseCategoryRepository
 import com.paykidscompose.common.result.DataResourceResult
+import com.paykidscompose.data.mapper.allowance.CategoryMapper
 import com.paykidscompose.data.model.allowance.CategoryDTO
 import com.paykidscompose.data.network.NetworkModule
 import com.paykidscompose.data.network.service.ExpenseCategoryApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class ExpenseCategoryRepositoryImpl(private val categoryApiService: ExpenseCategoryApiService = NetworkModule.provideExpenseCategoryApiService()) {
+class ExpenseCategoryRepositoryImpl(
+    private val categoryApiService: ExpenseCategoryApiService = NetworkModule.provideExpenseCategoryApiService()
+): ExpenseCategoryRepository {
 
-    suspend fun deleteExpenseCategory(category: String): DataResourceResult<Boolean> {
+    override suspend fun deleteExpenseCategory(category: String): DataResourceResult<Boolean> {
         return runCatching {
             categoryApiService.deleteExpenseCategory(category)
         }.fold(
@@ -18,7 +23,7 @@ class ExpenseCategoryRepositoryImpl(private val categoryApiService: ExpenseCateg
         )
     }
 
-    suspend fun saveExpenseCategory(category: String): DataResourceResult<Boolean> {
+    override suspend fun saveExpenseCategory(category: String): DataResourceResult<Boolean> {
         return runCatching {
             categoryApiService.saveExpenseCategory(category)
         }.fold(
@@ -27,12 +32,16 @@ class ExpenseCategoryRepositoryImpl(private val categoryApiService: ExpenseCateg
         )
     }
 
-    fun getExpenseCategoryList(): Flow<DataResourceResult<List<CategoryDTO>>> = flow {
+    override fun getExpenseCategoryList(): Flow<DataResourceResult<List<CategoryModel>>> = flow {
         emit(DataResourceResult.Loading)
         runCatching {
             categoryApiService.getExpenseCategoryList()
         }.fold(
-            onSuccess = { emit(DataResourceResult.Success(it.data))},
+            onSuccess = {
+                val models = it.data.map { dto ->
+                    CategoryMapper.mapToModel(dto)
+                }
+                emit(DataResourceResult.Success(models))},
             onFailure = { emit(DataResourceResult.Failure(it))}
         )
     }
