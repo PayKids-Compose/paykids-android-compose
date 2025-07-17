491686c65a439abfcdbdd2eafa878ae048125c08
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-07-17 15:22:12 +0900

    refactor: 유저 유스케이스 수정

--- common/src/main/java/com/paykidscompose/common/usecase/user/ReplaceNicknameUseCase.kt
+++ common/src/main/java/com/paykidscompose/common/usecase/user/ReplaceNicknameUseCase.kt
@@ -6,6 +6,12 @@ import com.paykidscompose.common.result.DataResourceResult
 class ReplaceNicknameUseCase(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(nickname: String): DataResourceResult<String> =
-        userRepository.replaceNickname(nickname)
+    suspend operator fun invoke(nickname: String): DataResourceResult<String> {
+
+        if (nickname.length < 2) {
+            return DataResourceResult.Failure(IllegalArgumentException("닉네임은 2자 이상이어야 합니다."))
+        }
+
+        return userRepository.replaceNickname(nickname)
+    }
 }
--- common/src/main/java/com/paykidscompose/common/usecase/user/SaveNicknameUseCase.kt
+++ common/src/main/java/com/paykidscompose/common/usecase/user/SaveNicknameUseCase.kt
@@ -6,6 +6,11 @@ import com.paykidscompose.common.result.DataResourceResult
 class SaveNicknameUseCase(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(nickname: String): DataResourceResult<String> =
-        userRepository.saveNickname(nickname)
+    suspend operator fun invoke(nickname: String): DataResourceResult<String> {
+        if (nickname.length < 2) {
+            return DataResourceResult.Failure(IllegalArgumentException("닉네임은 2자 이상이어야 합니다."))
+        }
+
+        return userRepository.saveNickname(nickname)
+    }
 }
