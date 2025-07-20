7516d2c49a962c93a05fb0b03894cb0de1b7f6ab
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-07-21 01:38:54 +0900

    feat: data, presentation 모듈 QuizMapper 작성

---
+++ presentation/src/main/java/com/paykidscompose/presentation/mapper/QuizUIModelMapper.kt
@@ -0,0 +1,25 @@
+package com.paykidscompose.presentation.mapper
+
+import com.paykidscompose.common.mapper.ModelMapper
+import com.paykidscompose.common.model.QuizModel
+import com.paykidscompose.presentation.model.QuizUIModel
+import com.paykidscompose.presentation.model.type.QuizType
+
+object QuizUIModelMapper: ModelMapper<QuizModel, QuizUIModel> {
+    override fun mapToModel(layerModel: QuizUIModel): QuizModel {
+        throw UnsupportedOperationException("QuizUIModel에서 QuizModel 변환은 지원하지 않습니다.")
+    }
+
+    override fun mapToLayerModel(model: QuizModel): QuizUIModel {
+        return QuizUIModel(
+            stage = model.stage,
+            number = model.number,
+            quizType = QuizType.valueOf(model.quizType),
+            question = model.question,
+            choices = model.choices.toSortedMap().values.toList(),
+            answer = model.answer,
+            imageUrl = model.choices.toSortedMap().values.toList(),
+            totalCount = model.count
+        )
+    }
+}
