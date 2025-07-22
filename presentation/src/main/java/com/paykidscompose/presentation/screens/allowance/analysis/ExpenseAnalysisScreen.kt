de0e22273e011e27569fd4e88c20e4a67403b107
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-07-23 02:33:23 +0900

    chore: import문 정리, 미사용 dimens 정리

--- presentation/src/main/java/com/paykidscompose/presentation/screens/allowance/analysis/ExpenseAnalysisScreen.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/screens/allowance/analysis/ExpenseAnalysisScreen.kt
@@ -39,10 +39,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.paykidscompose.common.enums.AllowanceType
 import com.paykidscompose.presentation.R
 import com.paykidscompose.presentation.dummy.AllowanceChartDTO
