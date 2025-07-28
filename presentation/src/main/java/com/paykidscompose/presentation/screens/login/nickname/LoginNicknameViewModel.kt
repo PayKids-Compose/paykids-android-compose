a1af8883362d5f1608b763b7727e9fa700dbe2a6
BossMG-github <sonmingyu23@naver.com>
2025-07-28 18:49:31 +0900

    refactor: 로그인 닉네임 화면 뷰모델 수정

--- presentation/src/main/java/com/paykidscompose/presentation/screens/login/nickname/LoginNicknameViewModel.kt
+++ presentation/src/main/java/com/paykidscompose/presentation/screens/login/nickname/LoginNicknameViewModel.kt
@@ -1,14 +1,16 @@
 package com.paykidscompose.presentation.screens.login.nickname
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.paykidscompose.common.exception.PayKidsException
 import com.paykidscompose.common.result.DataResourceResult
 import com.paykidscompose.common.usecase.user.SaveNicknameUseCase
+import com.paykidscompose.presentation.base.UIEvent
 import com.paykidscompose.presentation.base.UIState
 import com.paykidscompose.presentation.model.LoginNicknameUIModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -18,6 +20,9 @@ class LoginNicknameViewModel(
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(LoginNicknameUIState())
     val uiState = _uiState.asStateFlow()
+
+    private val _uiEvent = MutableSharedFlow<UIEvent>()
+    val uiEvent = _uiEvent.asSharedFlow()
 
 //    init {
 //        Log.e(TAG, "닉네임 뷰모델 생성!!!!!!!")
@@ -30,13 +35,13 @@ class LoginNicknameViewModel(
 
     fun updateNicknameInput(nickname: String) {
         _uiState.update {
-            it.copy(uiModel = it.uiModel.copy(nickname))
+            it.copy(uiModel = it.uiModel?.copy(nickname))
         }
     }
 
     fun saveNickname() {
         if (_uiState.value.isLoading) return
-        val nickname = _uiState.value.uiModel.nickname
+        val nickname = _uiState.value.uiModel?.nickname ?: ""
 
         _uiState.update {
             it.copy(isLoading = true, error = null)
@@ -45,25 +50,29 @@ class LoginNicknameViewModel(
         viewModelScope.launch {
             when (val result = saveNicknameUseCase(SaveNicknameUseCase.Params(nickname))) {
                 is DataResourceResult.Success -> {
-                    Log.d("LoginNicknameViewModel", "Success")
                     _uiState.update {
-                        it.copy(isLoading = false, isSaveSuccess = true)
+                        it.copy(isLoading = false)
                     }
+                    _uiEvent.emit(UIEvent.SuccessShowToast("닉네임 저장 완료! 회원가입 축하합니다!"))
                 }
 
                 is DataResourceResult.Failure -> {
                     _uiState.update {
                         it.copy(
                             isLoading = false,
-                            error = PayKidsException.SnackBarException(
-                                message = result.exception.message ?: "닉네임 저장 과정에 오류가 발생했습니다."
-                            )
+                            error = result.exception
                         )
                     }
                 }
 
                 DataResourceResult.DummyConstructor, DataResourceResult.Loading -> {}
             }
+        }
+    }
+
+    fun clearError() {
+        _uiState.update {
+            it.copy(error = null)
         }
     }
 
@@ -75,6 +84,5 @@ class LoginNicknameViewModel(
 data class LoginNicknameUIState(
     override val isLoading: Boolean = false,
     override val error: PayKidsException? = null,
-    val uiModel: LoginNicknameUIModel = LoginNicknameUIModel(""),
-    val isSaveSuccess: Boolean = false
+    val uiModel: LoginNicknameUIModel? = null
 ) : UIState()
