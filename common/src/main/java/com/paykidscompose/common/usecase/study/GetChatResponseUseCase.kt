30855f0f2ee1a77d142fc133f554437419a5d2b7
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-08-05 02:58:28 +0900

    feat: GetChatResponseUseCase 구현

--- common/src/main/java/com/paykidscompose/common/usecase/study/GetChatResponseUseCase.kt
+++ common/src/main/java/com/paykidscompose/common/usecase/study/GetChatResponseUseCase.kt
@@ -6,16 +6,55 @@ import com.paykidscompose.common.repositories.ChatRepository
 import com.paykidscompose.common.result.DataResourceResult
 import com.paykidscompose.common.usecase.base.FlowUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
 class GetChatResponseUseCase(
     private val chatRepository: ChatRepository
 ) : FlowUseCase<GetChatResponseUseCase.Params, DataResourceResult<ChatResponseModel>>() {
-    override fun execute(params: Params?): Flow<DataResourceResult<ChatResponseModel>> {
-        return if (params != null) {
-            chatRepository.getChatResponse(params.prompt)
-        } else {
-            flowOf(DataResourceResult.Failure(PayKidsException.ToastException(code = -1,"")))
+    override fun execute(params: Params?): Flow<DataResourceResult<ChatResponseModel>> = flow {
+        if (params == null) {
+            emit(
+                DataResourceResult.Failure(
+                    PayKidsException.ToastException(
+                        code = -1,
+                        message = "요청 파라미터가 없습니다."
+                    )
+                )
+            )
+            return@flow
+        }
+
+        when (val countResult = chatRepository.getChatCount()) {
+            is DataResourceResult.Success -> {
+                val count = countResult.data
+                if (count <= 0) { // 남은 요청 횟수가 0이 되면 GPT 요청 불가능
+                    emit(
+                        DataResourceResult.Failure(
+                            PayKidsException.ToastException(
+                                code = -1,
+                                message = "더 이상 요청할 수 없습니다."
+                            )
+                        )
+                    )
+                } else {
+                    emitAll(chatRepository.getChatResponse(params.prompt))
+                }
+            }
+
+            is DataResourceResult.Failure -> {
+                emit(
+                    DataResourceResult.Failure(
+                        PayKidsException.ToastException(
+                            code = -1,
+                            message = "채팅 카운트를 불러오는 데 실패했습니다."
+                        )
+                    )
+                )
+            }
+
+            DataResourceResult.DummyConstructor -> {}
+            DataResourceResult.Loading -> {}
         }
     }
 
