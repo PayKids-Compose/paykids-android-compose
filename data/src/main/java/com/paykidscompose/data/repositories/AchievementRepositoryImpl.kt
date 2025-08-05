b6f0c2846c3fa108afedf89587ed49630a09b17f
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2025-08-05 15:55:33 +0900

    feat: AchievementRepositoryImpl 작성

--- data/src/main/java/com/paykidscompose/data/repositories/AchievementRepositoryImpl.kt
+++ data/src/main/java/com/paykidscompose/data/repositories/AchievementRepositoryImpl.kt
@@ -1,16 +1,33 @@
 package com.paykidscompose.data.repositories
 
 import com.paykidscompose.common.exception.PayKidsException
+import com.paykidscompose.common.model.achievement.AchievementModel
+import com.paykidscompose.common.repositories.AchievementRepository
 import com.paykidscompose.common.result.DataResourceResult
-import com.paykidscompose.data.model.BaseResponse
-import com.paykidscompose.data.model.quest_achivement.AchievementDTO
+import com.paykidscompose.data.mapper.achievement.AchievementMapper
 import com.paykidscompose.data.network.service.AchievementApiService
-import com.paykidscompose.data.network.NetworkModule
 
-class AchievementRepositoryImpl(private val achievementApiService: AchievementApiService = NetworkModule.provideAchievementApiService()) {
-    suspend fun getAchievements(): DataResourceResult<BaseResponse<List<AchievementDTO>>> {
-        return runCatching { achievementApiService.getAchievements() }
-            .getOrElse { return DataResourceResult.Failure(PayKidsException.ToastException(code = -1, message = it.message ?: "")) }
-            .let { DataResourceResult.Success(it) }
+class AchievementRepositoryImpl(private val achievementApiService: AchievementApiService) :
+    AchievementRepository {
+    override suspend fun getAchievements(): DataResourceResult<List<AchievementModel>> {
+        return runCatching {
+            achievementApiService.getAchievements()
+        }.fold(
+            onSuccess = {
+                DataResourceResult.Success(
+                    it.data.map { achievementDTO ->
+                        AchievementMapper.mapToModel(achievementDTO)
+                    }
+                )
+            },
+            onFailure = {
+                DataResourceResult.Failure(
+                    PayKidsException.ToastException(
+                        code = -1,
+                        message = it.message ?: ""
+                    )
+                )
+            }
+        )
     }
 }
